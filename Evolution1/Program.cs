5ce4530df6ca6a412d49ba136f36dc59468e78d0
Auriel <Amadera@protonmail.com>
2021-09-20 09:30:08 +0300

    operator LINQ update. Algorithm Incapsulation fixed

--- Evolution1/Program.cs
+++ Evolution1/Program.cs
@@ -3,13 +3,8 @@
 using System;
 using System.IO;
 
-
 if (args.Length == 0) return;
-Directory.Delete(args[0], true);
-Directory.CreateDirectory(args[0]);
 
-
-var startFile = 0;
 
 (double Start, double End) range = (-20, -2.3);
 
@@ -20,30 +15,32 @@ PrepareInitialFile(function, range);
 
 
 var algorithm = new Algorithm
-{
-    Function = function,
-    Target = Target.Maximization,
-    Range = range,
-    PopulationDeviationStopFactor = 0.001,
-    InitialPopulationSize = 50,
-    LogFunc = Log
-};
+    (
+        function,
+        LogInfo,
+        range,
+        populationDeviationStopFactor: 0.001,
+        initialPopulationSize: 200
+    );
 
 
-var result = await algorithm.Solve();
+var result = algorithm.Solve();
 
 System.Console.WriteLine($"\n\tResult: {result}");
 
 
-void Log(LogInfo logInfo)
+void LogInfo(LogInfo logInfo)
 {
-    using var fileStream = new StreamWriter(File.Open($"{args[0]}\\Population-{startFile++}.tsv", FileMode.OpenOrCreate));
+    System.Console.WriteLine(logInfo.Message);
+    using var fileStream = new StreamWriter(File.Open($"{args[0]}\\Population-{logInfo.PopulationNumber}.tsv", FileMode.OpenOrCreate));
     foreach (var position in logInfo.Positions)
         fileStream.WriteLine($"{position.X}\t{position.Y}");
 }
 
 void PrepareInitialFile(TargetFunction targetFunction, (double Start, double End) range)
 {
+    Directory.Delete(args[0], true);
+    Directory.CreateDirectory(args[0]);
     var step = (range.End - range.Start) / ushort.MaxValue;
     using var file = new StreamWriter(File.Open($"{args[0]}\\initial.tsv", FileMode.OpenOrCreate));
     for (var i = 0; i < ushort.MaxValue; i++)
